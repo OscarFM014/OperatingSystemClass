cd4f11e2262c22216def464274d866fc8df64dba
ofernandezA07013362 <oscar.gohan2010@hotmail.com>
2021-03-17 01:31:02 +0000

    Práctica 2

--- xv6-public/init.c
+++ xv6-public/init.c
@@ -20,13 +20,20 @@ main(void)
   dup(0);  // stderr
 
   for(;;){
-    printf(1, "init: starting sh\n");
+	 // Cuando cambio "init: starting sh\n" por "STARTING JARVIS!" se muestra mi mensaje antes de iniciar
+	 // nuestro  init.c
+    printf(1, "STARTING JARVIS!");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
       exit();
     }
     if(pid == 0){
+		//Cuando corre sh todo sale normal y podemos entrar a ejecutar todo
+		//Cuando se cambia sh por ls se genera un bucle 
+		//infinito de ls o sea muestra los documentos una y otra vez y se ejecuta el mensaje de "STARTING JARVIS!"
+		//Cuando se cambia por no hay problema surge el mensaje modificado muchas veces mas init: exec sh failed\n
+		//Cuando cambio todo de regreso funciona perfecto como en un principio
       exec("sh", argv);
       printf(1, "init: exec sh failed\n");
       exit();
