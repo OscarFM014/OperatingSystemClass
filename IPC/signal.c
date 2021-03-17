6aa90c7df6acc145497d001a60251c4ffa6f6601
ofernandezA07013362 <oscar.gohan2010@hotmail.com>
2021-03-17 03:01:53 +0000

    Practica 3

---
+++ IPC/signal.c
@@ -0,0 +1,25 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+int globalStatus;
+
+void signalHandler(int sig){
+	printf("Recibe signal %d\n", sig);
+	globalStatus = sig;
+}
+
+int main(){
+	signal(2,signalHandler);
+	while(1){
+		printf("Trabajando...\n");
+		if(globalStatus == 2 || globalStatus == 15){
+			printf("Ya voy a terminar \n");
+			break;
+		}
+		sleep(1);
+	}
+	
+	return 0;
+}
